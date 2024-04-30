e492a3870df38f14da3993ac116cb9915fa8f0c1
LENI4C <timibraperetei74@gmail.com>
2024-04-30 12:21:39 +0100

    file structure edit + Header component

---
+++ components/Header.jsx
@@ -0,0 +1,15 @@
+const Header = () => {
+    return (
+        <header>
+            <div className="logo">
+                <img src="/assets/images/troll-face.png" alt="troll face logo" />
+                <h1> Meme Generator</h1>
+            </div>
+            <div className="header-cred">
+                <a href="https://www.github.com/LENI4C"> leni4c </a>
+            </div>
+        </header>
+    )
+};
+
+export default Header;
