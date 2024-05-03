430cdcdc23976a7eb49448eb01efd1cb73f1d47a
LENI4C <timibraperetei74@gmail.com>
2024-05-03 03:23:54 +0100

    Memeimage logic check

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,3 +1,5 @@
+import memedata from "../component data/memedata";
+
 const Main = () => {
     return (
         <section>
@@ -13,19 +15,47 @@ export const Form = () => {
                 {/* left input text-box */}
                 <div className="input-text-box">
                     <label>Top text</label>
-                    <input type="text" name="" id="" placeholder="first half of phrase"/>
+                    <input
+                        type="text"
+                        name=""
+                        id=""
+                        placeholder="first half of phrase"
+                    />
                 </div>
 
                 {/* right input text-box */}
                 <div className="input-text-box">
                     <label>Bottom text</label>
-                    <input type="text" name="" id="" placeholder="last half of phrase"/>
+                    <input
+                        type="text"
+                        name=""
+                        id=""
+                        placeholder="last half of phrase"
+                    />
                 </div>
             </div>
 
-            <button type="submit">Get a new meme image 🌄 </button>
+            <button type="submit" onClick={Meme}>
+                Get a new meme image 🌄
+            </button>
         </form>
     );
+};
+
+const Meme = (e) => {
+    e.preventDefault();
+    const renderMeme = memedata.map((data) => (
+        <Memeinfo key={data.id} {...data} />
+    ));
+    return (
+        <div>
+            {renderMeme}
+        </div>
+    );
+};
+
+const Memeinfo = ({url}) => {
+    return <img src={url} alt="meme-image" />;
 };
 
 export default Main;
